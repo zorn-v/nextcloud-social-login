7e741cdd1942866b78e2179049684aa4e67acff9
Zorn <zorn84@bk.ru>
2018-04-11 20:01:00 +1000

    Changed oauth login url

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -10,6 +10,6 @@
 return [
     'routes' => [
 	   ['name' => 'settings#saveAdmin', 'url' => '/settings/save-admin', 'verb' => 'POST'],
-       ['name' => 'oAuth#login', 'url' => '/oauth/login', 'verb' => 'GET'],
+       ['name' => 'oAuth#login', 'url' => '/oauth/{provider}', 'verb' => 'GET'],
     ]
 ];
