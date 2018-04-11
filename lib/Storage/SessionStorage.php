57016018ec42cf4c06f09e3a0d31ae038841267f
Zorn <zorn84@bk.ru>
2018-04-11 22:36:32 +1000

    Fixed session issue

--- lib/Storage/SessionStorage.php
+++ lib/Storage/SessionStorage.php
@@ -2,73 +2,18 @@
 
 namespace OCA\SocialLogin\Storage;
 
-use OCP\ISession;
-use Hybridauth\Storage\StorageInterface;
+use Hybridauth\Storage\Session;
 
-class SessionStorage implements StorageInterface
+class SessionStorage extends Session
 {
-    /** @var ISession */
-    private $session;
-    private $ns = 'HA-STORAGE';
-
-    public function __construct(ISession $session)
-    {
-        $this->session = $session;
-    }
-
-    /**
-    * {@inheritdoc}
-    */
-    public function get($key)
-    {
-        error_log("GET $key");
-        $values = $this->session->get($this->ns);
-        return $values[$key];
-    }
-
     /**
     * {@inheritdoc}
     */
     public function set($key, $value)
     {
-        error_log("SET $key = $value");
-        $values = $this->session->get($this->ns);
-        $values[$key] = $value;
-        $this->session->set($this->ns, $values);
-    }
-
-    /**
-    * {@inheritdoc}
-    */
-    public function delete($key)
-    {
-        error_log("DELETE $key");
-        $values = $this->session->get($this->ns);
-        unset($values[$key]);
-        $this->session->set($this->ns, $values);
-    }
-
-    /**
-    * {@inheritdoc}
-    */
-    public function deleteMatch($key)
-    {
-        error_log("DELETE MATCH $key");
-        $values = $this->session->get($this->ns);
-        foreach ($values as $k=>$v) {
-            if (strstr($k, $key)) {
-                unset($values[$key]);
-            }
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            session_start();
         }
-        $this->session->set($this->ns, $values);
-    }
-
-    /**
-    * {@inheritdoc}
-    */
-    public function clear()
-    {
-        error_log("CLEAR");
-        $this->session->set($this->ns, []);
+        parent::set($key, $value);
     }
 }
