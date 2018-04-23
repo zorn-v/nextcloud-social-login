d3ab5e3da87ffa2014962b251a24395ba0b1e0a9
Yan <yan@sutoiku.com>
2018-04-23 15:15:27 +0200

    Get profile from id_token attribute

--- lib/Provider/OAuth2.php
+++ lib/Provider/OAuth2.php
@@ -3,6 +3,8 @@
 namespace OCA\SocialLogin\Provider;
 
 use Hybridauth\User;
+use Hybridauth\Data;
+use Hybridauth\Exception\Exception;
 
 class OAuth2 extends \Hybridauth\Adapter\OAuth2
 {
@@ -25,8 +27,34 @@ class OAuth2 extends \Hybridauth\Adapter\OAuth2
         return $this->authenticateFinish();
     }
 
+    protected function validateAccessTokenExchange($response)
+    {
+        $collection = parent::validateAccessTokenExchange($response);
+        if ($collection->exists('id_token')) {
+            $idToken = $collection->get('id_token');
+            //get payload from id_token
+            $parts = explode('.', $idToken);
+            list($headb64, $payload) = $parts;
+            $data = base64_decode($payload);
+            $this->storeData('user_data', $data);
+        }
+        else{
+          throw new Exception('No id_token was found.');
+        }
+        return $collection;
+    }
+
+
     public function getUserProfile(){
+      $userData = $this->getStoredData('user_data');
+      $user = json_decode($userData);
+      $data = new Data\Collection($user);
+
       $userProfile = new User\Profile();
+      $userProfile->identifier  = $data->get('sub');
+      $userProfile->email       = $data->get('email');
+      $userProfile->displayName = $data->get('name');
+
       return $userProfile;
     }
 }
