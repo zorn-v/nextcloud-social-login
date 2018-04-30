a025338c8dc778771a397cfdfd7d5719eafe39f6
Zorn <zorn84@bk.ru>
2018-04-30 20:20:44 +1000

    Universal release script

--- release.sh
+++ release.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+GITHUB_REPO=zorn-v/nextcloud-social-login
+APP_NAME=sociallogin
+
 cd `dirname $0`
 
 git diff --quiet --exit-code
@@ -8,7 +11,7 @@ git diff --quiet --exit-code
 . .credentials
 
 VERSION=v`grep '<version>' appinfo/info.xml | sed 's/[^0-9.]//g'`
-UPLOAD_URL=`curl -sH "Authorization: token $GITHUB_TOKEN" -d "{\"tag_name\":\"$VERSION\"}" https://api.github.com/repos/zorn-v/nextcloud-social-login/releases | grep '"upload_url"' | sed 's/.*"\(https:.*\){.*/\1/'`
+UPLOAD_URL=`curl -sH "Authorization: token $GITHUB_TOKEN" -d "{\"tag_name\":\"$VERSION\"}" https://api.github.com/repos/$GITHUB_REPO/releases | grep '"upload_url"' | sed 's/.*"\(https:.*\){.*/\1/'`
 [ -z "$UPLOAD_URL" ] && echo Can not get upload url && exit 1
 
 git checkout -b release
@@ -19,14 +22,14 @@ sed -i '/<description><\/description>/ {
   d
 }' appinfo/info.xml
 git commit -am 'Release'
-git archive release --prefix=sociallogin/ -o release.tar.gz
+git archive release --prefix=$APP_NAME/ -o release.tar.gz
 git checkout master
 git branch -D release
 
 curl -sH "Authorization: token $GITHUB_TOKEN" -H 'Content-Type: application/octet-stream' --data-binary '@release.tar.gz' ${UPLOAD_URL}?name=release.tar.gz > /dev/null
-DOWNLOAD_URL=https://github.com/zorn-v/nextcloud-social-login/releases/download/$VERSION/release.tar.gz
+DOWNLOAD_URL=https://github.com/$GITHUB_REPO/releases/download/$VERSION/release.tar.gz
 
-SIG=`openssl dgst -sha512 -sign ~/.nextcloud/certificates/sociallogin.key release.tar.gz | openssl base64 -A`
+SIG=`openssl dgst -sha512 -sign ~/.nextcloud/certificates/$APP_NAME.key release.tar.gz | openssl base64 -A`
 curl -X POST -sH "Authorization: Token $NC_TOKEN" -H 'Content-Type: application/json' -d "{\"download\":\"$DOWNLOAD_URL\",\"signature\":\"$SIG\"}" https://apps.nextcloud.com/api/v1/apps/releases
 
 rm -f release.tar.gz
