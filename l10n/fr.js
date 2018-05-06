1c710d3697823ba2c62cc9f0936e5fe53388f95f
thomas-lb <thomas-lb@users.noreply.github.com>
2018-05-06 21:27:13 +0200

    Add French translation

---
+++ l10n/fr.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+  "sociallogin",
+  {
+    "Settings for social login successfully saved": "Paramètres sauvegardés avec succès",
+    "Do you realy want to remove this {providerType} provider ?": "Voulez-vous vraiment supprimer ce fournisseur {providerType} ?",
+    "Some error occurred while saving settings": "Erreur lors de la sauvegarde des paramètres",
+    "Confirm remove": "Confirmer la suppression"
+  },
+"nplurals=2; plural=(n > 1);");
