6a04ad9e122f4ced688cced8155d018c37134803
Steven Tang <mrsteventang@outlook.com>
2019-01-08 20:00:04 +0800

    Create zh_CN.js

---
+++ l10n/zh_CN.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+  "sociallogin",
+  {
+    "Settings for social login successfully saved": "保存成功",
+    "Do you realy want to remove {providerTitle} provider ?": "您确定要刪除{providerTitle}吗？",
+    "Some error occurred while saving settings": "保存设置时发生错误",
+    "Confirm remove": "确认删除"
+  },
+"nplurals=1; plural=0;");
