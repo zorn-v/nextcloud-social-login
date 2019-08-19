a0efbd43a7f917338884da04e537c6cfe56643f7
zorn <zorn84@bk.ru>
2019-08-19 15:50:34 +1000

    Add Brazilian Portuguese translation

---
+++ l10n/pt_BR.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+  "sociallogin",
+  {
+    "Settings for social login successfully saved": "Configurações de login social salvas com sucesso",
+    "Do you realy want to remove {providerTitle} provider ?": "Tem certeza de que deseja remover o provedor {providerTitle}?",
+    "Some error occurred while saving settings": "Ocorreu um erro ao salvar as configurações",
+    "Confirm remove": "Confirmar remoção"
+  },
+"nplurals=2; plural=(n > 1);");
