be436957dbf5ea2fd1f87150135f7c6f9716ca3d
Jochen <rothjochen@gmail.com>
2019-09-05 22:33:06 +0200

    Add german translation
    Signed-off-by: Jochen <rothjochen@gmail.com>

--- l10n/de.js
+++ l10n/de.js
@@ -1,6 +1,7 @@
 OC.L10N.register(
   "sociallogin",
   {
+    "Log in with": "Anmelden mit",
     "Settings for social login successfully saved": "Die Einstellungen für Social Login wurden erfolgreich gespeichert",
     "Do you realy want to remove {providerTitle} provider ?": "Möchten Sie den Provider {providerTitle} wirklich entfernen?",
     "Some error occurred while saving settings": "Es trat ein Fehler beim Speichern der Einstellungen auf",
