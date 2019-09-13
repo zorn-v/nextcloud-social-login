0c4dfb534a2881d1a4e99f8488c0e0de82e54bad
zorn <zorn7@yandex.ru>
2019-09-13 15:04:56 +1000

    Telegram login button before alternative logins form

--- js/telegram.js
+++ js/telegram.js
@@ -2,5 +2,5 @@ jQuery(function ($) {
   var tgData = $('tg-data');
   var login = tgData.data('login');
   var redirectUrl = tgData.data('redirect-url');
-  $('form[name="login"]').after('<script src="https://telegram.org/js/telegram-widget.js?5" data-size="large" data-telegram-login="'+login+'" data-auth-url="'+redirectUrl+'"/>');
+  $('#alternative-logins').before('<script src="https://telegram.org/js/telegram-widget.js?5" data-size="large" data-telegram-login="'+login+'" data-auth-url="'+redirectUrl+'"/>');
 });
