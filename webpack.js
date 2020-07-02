84da224fccda95ffb112acacd35a54561d664b72
zorn <zorn7@yandex.ru>
2020-07-03 02:52:32 +1000

    Do not use jquery in settings.js

--- webpack.js
+++ webpack.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
   mode: 'production',
 	entry: {
-		'personal': path.join(__dirname, 'src', 'personal.js'),
+    'personal': path.join(__dirname, 'src', 'personal.js'),
+    'settings': path.join(__dirname, 'src', 'settings.js'),
 	},
 	output: {
 		path: path.resolve(__dirname, './js/'),
