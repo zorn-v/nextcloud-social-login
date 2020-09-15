cc591f87d8602b8dd4cc8f97ea521718706dbd6c
blkgithub <58100228+blkgithub@users.noreply.github.com>
2020-09-15 19:07:44 -0300

    Now update user street address if exists (#197)
    Now update user street address if exists

--- lib/Provider/CustomOpenIDConnect.php
+++ lib/Provider/CustomOpenIDConnect.php
@@ -36,6 +36,9 @@ class CustomOpenIDConnect extends CustomOAuth2
         $userProfile->displayName = $data->get('name') ?: $data->get('preferred_username');
         $userProfile->photoURL    = $data->get('picture');
         $userProfile->email       = $data->get('email');
+        if($data->exists('street_address')){
+            $userProfile->address       = $data->get('street_address');
+        }
         if (null !== $groups = $this->getGroups($data)) {
             $userProfile->data['groups'] = $groups;
         }
