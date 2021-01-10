d9159836a4567bdca0b6ff01e13cc649dd056ec7
zorn <zorn7@yandex.ru>
2021-01-11 04:14:42 +1000

    Use NC 20 bootstrapping

--- appinfo/app.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require __DIR__ . '/../3rdparty/autoload.php';
-
-$app = \OC::$server->query(OCA\SocialLogin\AppInfo\Application::class);
-$app->register();
--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -5,6 +5,9 @@ namespace OCA\SocialLogin\AppInfo;
 use OCA\SocialLogin\Db\SocialConnectDAO;
 use OCA\SocialLogin\Service\ProviderService;
 use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\IConfig;
 use OCP\IL10N;
 use OCP\IRequest;
@@ -15,7 +18,7 @@ use OCP\IUserManager;
 use OCP\IUserSession;
 use OCP\Util;
 
-class Application extends App
+class Application extends App implements IBootstrap
 {
     private $appName = 'sociallogin';
 
@@ -24,15 +27,18 @@ class Application extends App
         parent::__construct($this->appName);
     }
 
-    public function register()
+    public function register(IRegistrationContext $context): void
+    {
+        require __DIR__ . '/../../3rdparty/autoload.php';
+    }
+
+    public function boot(IBootContext $context): void
     {
         Util::addStyle($this->appName, 'styles');
+
         $l = $this->query(IL10N::class);
-
         $config = $this->query(IConfig::class);
-
         $this->query(IUserManager::class)->listen('\OC\User', 'preDelete', [$this, 'preDeleteUser']);
-
         $userSession = $this->query(IUserSession::class);
         if ($userSession->isLoggedIn()) {
             $uid = $userSession->getUser()->getUID();
