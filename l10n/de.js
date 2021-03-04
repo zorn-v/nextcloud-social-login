c47d684ddc8296750c08ab4748f15d2e00b1b00a
Daniel Richter <daniel.richter@hpi.de>
2021-03-04 18:36:40 +0100

    German localization

--- l10n/de.js
+++ l10n/de.js
@@ -1,19 +1,24 @@
 OC.L10N.register(
   "sociallogin",
   {
-    "Disable auto create new users": "Automatische Erstellung neuer Benutzer deaktivieren",
-    "Allow users to connect social logins with their account": "Benutzern erlauben externe Konten mit Ihrem Konto zu verknüpfen",
-    "Prevent creating an account if the email address exists in another account": "Verhindere die Erstellung eines Kontos, wenn die E-Mail bereits in einem anderen Konto hinterlegt ist",
-    "Update user profile every login": "Aktualisiere Benutzer-Profil nach jeder Anmeldung",
-    "Do not prune not available user groups on login": "Bei der Anmeldung nicht verfügbare Gruppen nicht löschen",
+    "Disable auto create new users": "Automatisches Anlegen neuer Benutzer deaktivieren",
+    "Create users with disabled account": "Benutzer mit einem deaktivierten Konto erstellen",
+    "Allow users to connect social logins with their account": "Benutzern erlauben, externe Konten mit Ihrem Konto zu verknüpfen",
+    "Prevent creating an account if the email address exists in another account": "Anlegen eines Kontos verhindern, wenn die Email-Adresse bereits von einem anderen Konto verwendet wird",
+    "Update user profile every login": "Benutzerprofil nach jeder Anmeldung aktualisieren",
+    "Do not prune not available user groups on login": "Nicht vorhandene Benutzergruppen bei der Anmeldung nicht wieder löschen",
     "Automatically create groups if they do not exists": "Nicht existierende Gruppen automatisch erstellen",
-    "Restrict login for users without mapped groups": "Anmeldung auf Benutzer in festgelegten Gruppen beschränken",
+    "Restrict login for users without mapped groups": "Verhindern, dass sich Benutzer ohne zugeordnete Gruppe anmelden können",
+    "Disable notify admins about new users": "Benachrichtigung von Admins über neue Benutzer deaktivieren",
+    "Hide default login (can be made visible with /login?showDefault=true)": "Standard-Login ausblenden (kann durch /login?showDefault=true wieder sichtbar gemacht werden)",
 
     "None": "Keine",
     "Save": "Speichern",
+    "Allow login only from specified domain": "Nur von der angegebenen Domäne",
+    "Allow login only for specified organizations": "Nur für Mitglieder der Organisationen",
 
     "Settings for social login successfully saved": "Die Einstellungen für Social Login wurden erfolgreich gespeichert",
-    "Do you realy want to remove {providerTitle} provider ?": "Möchten Sie den Provider {providerTitle} wirklich entfernen?",
+    "Do you realy want to remove {providerTitle} provider ?": "Möchten Sie den Autorisierungsdienst {providerTitle} wirklich entfernen?",
     "Some error occurred while saving settings": "Es trat ein Fehler beim Speichern der Einstellungen auf",
     "Confirm remove": "Entfernen bestätigen"
   },
