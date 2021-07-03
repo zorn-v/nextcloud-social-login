a5f894572ce6272770f050fa015afe95cd02e61e
zorn <zorn7@yandex.ru>
2021-07-03 23:35:49 +1000

    Fix telegram include without any other alt logins

--- js/telegram.js
+++ js/telegram.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', function () {
   var login = tgData.dataset.login
   var redirectUrl = tgData.dataset.redirectUrl
 
-  var altLogins = document.getElementById('alternative-logins')
-  if (altLogins) {
+  var mainEl = document.getElementsByTagName('main')[0]
+  if (mainEl) {
     var script = document.createElement('script')
     script.src = 'https://telegram.org/js/telegram-widget.js?9'
     script.dataset.size = 'large'
     script.dataset.telegramLogin = login
     script.dataset.authUrl = redirectUrl
-    altLogins.parentNode.insertBefore(script, altLogins.nextElementSibling)
+    mainEl.appendChild(script)
   }
 })
