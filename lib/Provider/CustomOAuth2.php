5698687c6845a8d8bdbba85ee326a6d64bc80c56
zorn <zorn7@yandex.ru>
2022-01-26 23:15:09 +1000

    Fix connect account with several custom providers

--- lib/Provider/CustomOAuth2.php
+++ lib/Provider/CustomOAuth2.php
@@ -5,10 +5,24 @@ namespace OCA\SocialLogin\Provider;
 use Hybridauth\Adapter\OAuth2;
 use Hybridauth\Data;
 use Hybridauth\Exception\UnexpectedApiResponseException;
+use Hybridauth\HttpClient\HttpClientInterface;
+use Hybridauth\Logger\LoggerInterface;
+use Hybridauth\Storage\StorageInterface;
 use Hybridauth\User;
 
 class CustomOAuth2 extends OAuth2
 {
+
+    public function __construct(
+        $config = [],
+        HttpClientInterface $httpClient = null,
+        StorageInterface $storage = null,
+        LoggerInterface $logger = null
+    ) {
+        parent::__construct($config, $httpClient, $storage, $logger);
+        $this->providerId = $this->clientId;
+    }
+
     /**
      * @return User\Profile
      * @throws UnexpectedApiResponseException
