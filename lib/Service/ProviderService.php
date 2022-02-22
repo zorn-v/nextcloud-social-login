f445202826db0258a29dff18318c1c067104304e
zorn <zorn7@yandex.ru>
2022-02-22 23:28:29 +1000

    Call setSystemEMailAddress if exists

--- lib/Service/ProviderService.php
+++ lib/Service/ProviderService.php
@@ -459,7 +459,11 @@ class ProviderService
             if (method_exists($user, 'setSystemEMailAddress')) {
                 $user->setSystemEMailAddress((string)$profile->email);
             } else {
-                $user->setEMailAddress((string)$profile->email);
+                if (method_exists($user, 'setSystemEMailAddress')) {
+                    $user->setSystemEMailAddress((string)$profile->email);
+                } else {
+                    $user->setEMailAddress((string)$profile->email);
+                }
             }
 
             if ($profile->photoURL) {
