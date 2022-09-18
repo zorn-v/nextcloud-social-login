3e1f12d46800cce01292a929665665e6d3f1b78f
zorn <zorn7@yandex.ru>
2022-09-19 05:25:44 +1000

    Return empty response instead of "null" in api

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace OCA\SocialLogin\Controller;
 
 use OCP\AppFramework\ApiController as BaseController;
+use OCP\AppFramework\Http\Response;
 use OCP\IConfig;
 use OCP\IRequest;
 
@@ -28,5 +29,6 @@ class ApiController extends BaseController
     public function setConfig($key, $config)
     {
         $this->config->setAppValue($this->appName, $key, $config);
+        return new Response();
     }
 }
